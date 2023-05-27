2857e628e64e57306f11e623ff14a0d77970c2e2
JordanLong1 <jordanleelong5@gmail.com>
2023-05-26 23:18:09 -0700

    made form that can create users, added migration for username

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
+import { Form, useActionData } from "@remix-run/react";
 import { PrismaClient } from "@prisma/client";
 import { useLoaderData } from "@remix-run/react";
+import { useTransition } from "react";0
 
 export async function loader() {
   const prisma = new PrismaClient()
@@ -8,16 +10,43 @@ export async function loader() {
   await prisma.$disconnect();
   return allUsers
 }
+
+export async function action({request}) {
+  console.log('req', request)
+  const form = await request.formData(); 
+  const prisma = await new PrismaClient();
+
+  const allUsers = await prisma.user.create({
+    data: {email: form.get("email"), username: form.get("username"), name: form.get("name")}
+  })
+
+  console.log('all users', allUsers)
+  await prisma.$disconnect();
+  return true
+
+
+}
+
 export default function Index() {
   const users = useLoaderData();
+  console.log('users', users)
+  const {state} = useTransition();
+  const data = useActionData<typeof action>();
+
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-    <div>Hello world</div>
-    {users.map((user) => {
-      const {name, id} = user;
-      return <li key={id}>{name}</li>
-    })}
+    <div>Create a new user</div>
+    <Form method="post">
+      <label htmlFor="username">Username: </label>
+        <input type="text" name="username"  />
+        <label htmlFor="email">Email: </label>
+        <input type="text" name="email" />
+        <label htmlFor="name">Name: </label>
+        <input type="text" name="name" />
+        <button type="submit">Create user</button>
+    </Form>
+    <p>{data ? data : 'No data'}</p>
     </div>
   );
 }
